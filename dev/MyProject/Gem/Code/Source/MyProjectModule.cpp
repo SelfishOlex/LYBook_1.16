04c190eb18c26e77ad74f40e9f1eb07a3f1b9647
Olex Lozitskiy <olex.developer@gmail.com>
2018-11-16 13:00:24 -0800

    Chapter 4. Writing Your Own Components

--- dev/MyProject/Gem/Code/Source/MyProjectModule.cpp
+++ dev/MyProject/Gem/Code/Source/MyProjectModule.cpp
@@ -1,9 +1,7 @@
-
-
 #include <AzCore/Memory/SystemAllocator.h>
 #include <AzCore/Module/Module.h>
-
 #include <MyProjectSystemComponent.h>
+#include <MyComponent.h>
 
 namespace MyProject
 {
@@ -11,22 +9,27 @@ namespace MyProject
         : public AZ::Module
     {
     public:
-        AZ_RTTI(MyProjectModule, "{D42F0849-C3A8-4476-92BF-0D97340B067C}", AZ::Module);
-        AZ_CLASS_ALLOCATOR(MyProjectModule, AZ::SystemAllocator, 0);
+        AZ_RTTI(MyProjectModule,
+            "{D42F0849-C3A8-4476-92BF-0D97340B067C}", AZ::Module);
+        AZ_CLASS_ALLOCATOR(MyProjectModule,
+            AZ::SystemAllocator, 0);
 
         MyProjectModule()
             : AZ::Module()
         {
-            // Push results of [MyComponent]::CreateDescriptor() into m_descriptors here.
+            // Push results of [MyComponent]::CreateDescriptor()
+            // into m_descriptors here.
             m_descriptors.insert(m_descriptors.end(), {
                 MyProjectSystemComponent::CreateDescriptor(),
+                MyComponent::CreateDescriptor(),
             });
         }
 
         /**
          * Add required SystemComponents to the SystemEntity.
          */
-        AZ::ComponentTypeList GetRequiredSystemComponents() const override
+        AZ::ComponentTypeList
+            GetRequiredSystemComponents() const override
         {
             return AZ::ComponentTypeList{
                 azrtti_typeid<MyProjectSystemComponent>(),
@@ -37,5 +40,6 @@ namespace MyProject
 
 // DO NOT MODIFY THIS LINE UNLESS YOU RENAME THE GEM
 // The first parameter should be GemName_GemIdLower
-// The second should be the fully qualified name of the class above
-AZ_DECLARE_MODULE_CLASS(MyProject_ebc264b367f841b6840b65ec4bd91a29, MyProject::MyProjectModule)
+// The second should be the fully qualified name of the class
+AZ_DECLARE_MODULE_CLASS(MyProject_ebc264b367f841b6840b65ec4bd91a29,
+    MyProject::MyProjectModule)
